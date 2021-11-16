49056cfff3a4f38e2a924a09e3f0c8d7ff369eb7
Dmitriy Novikov <feakstem@gmail.com>
2021-11-16 09:59:40 +0300

    Добавьте файлы проекта.

---
+++ BuildersApp_Novikov_3ISP11_13/Views/AuthWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using BuildersApp_Novikov_3ISP11_13.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BuildersApp_Novikov_3ISP11_13.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+
+        Entities Entities = new Entities();
+        public static User authUser;
+        public AuthWindow()
+        {
+            InitializeComponent();
+            AuthWin = this;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            authUser = Entities.User.FirstOrDefault(i => i.Login == tboxLogin.Text && i.Password.Equals(tboxPassword.Text));
+            if(authUser != null)
+            {
+                if (authUser.Password == tboxPassword.Text)
+                {
+                    MainMenuWindow mainMenuWindow = new MainMenuWindow();
+                    mainMenuWindow.Show();
+                    Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Неверный пароль, повторите попытку", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }    
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден, повторите попытку", "Пользователь не найден", MessageBoxButton.OK, MessageBoxImage.Error);
+            } 
+        }
+        private void Drag(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                AuthWin.DragMove();
+        }
+    }
+}
---
+++ BuildersApp_Novikov_3ISP11_13/Views/MainMenuWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using BuildersApp_Novikov_3ISP11_13.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BuildersApp_Novikov_3ISP11_13.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenuWindow.xaml
+    /// </summary>
+    public partial class MainMenuWindow : Window
+    {
+
+        Entities Entities = new Entities();
+        public Post role;
+        public MainMenuWindow()
+        {
+            InitializeComponent();
+            MainWin = this;
+            role = Entities.Post.Find(AuthWindow.authUser.Post.IdPost);
+            if (role.PostName.Equals("Администратор"))
+                tblockTitle.Text = "Окно администратора";
+            else if(role.PostName.Equals("Менеджер"))
+                tblockTitle.Text = "Окно менеджера";
+        }
+
+        private void Drag(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                MainWin.DragMove();
+        }
+
+        private void btnMinWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaxWindow_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+
+            else
+                this.WindowState = WindowState.Normal;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
